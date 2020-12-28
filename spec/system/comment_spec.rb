e27dad979aefcc189ec7215f765429903b6dd001
kondo0602 <kondo8363@gmail.com>
2020-12-28 12:15:55 +0900

    コメントの投稿に失敗した時のflash追加

--- spec/system/comment_spec.rb
+++ spec/system/comment_spec.rb
@@ -1,28 +1,41 @@
 require 'rails_helper'
 
 describe 'コメント管理機能', type: :system do
-  let(:user_a) { create(:user) }
-  let(:user_b) { create(:user) }
-  let!(:item) { create(:item, user: user_a) }
+  before do
+    @user = build(:user)
+    @user.image = fixture_file_upload('/files/user.jpg')
+    @user.save!
+    @item = build(:item, user_id: @user.id)
+    @item.image = fixture_file_upload('/files/item1.jpg')
+    @item.save!
+  end
 
   describe 'コメント新規作成機能' do
     before do
-      visit new_user_path
-      fill_in 'user[name]', with: 'test'
-      fill_in 'user[email]', with: 'a@example.com'
-      select '宮城県', from: 'user[address]'
-      fill_in 'user[password]', with: 'password'
-      fill_in 'user[password_confirmation]', with: 'password'
-      click_on 'ユーザ登録'
-      visit item_path(item)
+      # ログインする
+      visit login_path
+      fill_in 'session[email]', with: @user.email
+      fill_in 'session[password]', with: 'password'
+      click_on 'ログインする'
     end
 
     context 'コメントが入力されている場合' do
       it '商品詳細ページにリダイレクトされ、適切なflashが表示されていること' do
+        visit item_path(@item)
         fill_in 'comment[content]', with: 'コメントです。'
         click_on 'コメントする'
-        expect(current_path).to eq item_path(item)
+        expect(current_path).to eq item_path(@item)
         expect(page).to have_content 'コメントを投稿しました'
+      end
+    end
+
+    context 'コメントが入力されていない場合' do
+      it '商品詳細ページにリダイレクトされ、適切なflashが表示されていること' do
+        visit item_path(@item)
+        fill_in 'comment[content]', with: ''
+        click_on 'コメントする'
+        expect(current_path).to eq item_path(@item)
+        expect(page).to have_content 'コメントの投稿に失敗しました'
       end
     end
   end
