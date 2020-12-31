52d672bcfeda3b07885db052327dc735a10097de
kondo0602 <kondo8363@gmail.com>
2020-12-31 09:28:51 +0900

    商品編集画面へのリンクが切れていた事象を修正

--- spec/system/items_spec.rb
+++ spec/system/items_spec.rb
@@ -14,4 +14,20 @@ RSpec.describe '商品管理機能', type: :system do
       expect(page).to have_content @item.name
     end
   end
+
+  describe '商品編集機能' do
+    before do
+      # ログインする
+      visit login_path
+      fill_in 'session[email]', with: @user.email
+      fill_in 'session[password]', with: 'password'
+      click_on 'ログインする'
+    end
+
+    it '商品編集ページに遷移できること' do
+      visit item_path(@item)
+      click_on 'この商品を編集する'
+      expect(current_path).to eq edit_item_path(@item)
+    end
+  end
 end
