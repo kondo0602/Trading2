a909e7291e03a115000057b255af37260bb687b5
kondo0602 <kondo8363@gmail.com>
2021-02-09 20:07:28 +0900

    ajaxによるコメントの非同期更新

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,9 @@ class CommentsController < ApplicationController
 
   def create
     @comment = current_user.comments.build(comment_params)
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
     if @comment.save
+      @comments = Comment.where(item_id: params[:item_id])
       flash[:success] = 'コメントを投稿しました'
     else
       flash[:danger] = 'コメントの投稿に失敗しました'
