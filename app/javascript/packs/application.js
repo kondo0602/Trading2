6e2597dd2f4072f96af6c668e3e0b92b7813582e
kondo0602 <kondo8363@gmail.com>
2021-02-10 06:22:40 +0900

    本番でもajaxが動くように修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,15 @@
+//importはES6で決められたモジュール読み込みの仕様
+//requireはサーバサイドで動くjavascriptの読み込み
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-// import "bootstrap"
+import "bootstrap"
 
-var jQuery = require('jquery')
-global.$ = global.jQuery = jQuery;
-window.$ = window.jQuery = jQuery;
+// var jQuery = require('jquery')
+// global.$ = global.jQuery = jQuery;
+// window.$ = window.jQuery = jQuery;
 
 //= require jquery
 //= require rails-ujs
